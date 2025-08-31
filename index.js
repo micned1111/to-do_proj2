d7c0548b903a64a536d109e2c2c6c36e6952ca3d
Michael Nedgård Isaksen <michael.n.i@icloud.com>
2025-08-31 13:29:00 +0200

    Made the code in js more readable/visually appealing

--- index.js
+++ index.js
@@ -17,40 +17,42 @@ let taskArr = JSON.parse(localStorage.getItem("tasks-data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
+	if (Object.keys(currentTask).length === 0) {
+		const taskObj = {
+			id: `${titleInput.value
+				.toLowerCase()
+				.split(" ")
+				.join("-")}-${Date.now()}`,
+			title: `${titleInput.value}`,
+			date: `${dateInput.value}`,
+			description: `${descriptionInput.value}`,
+		};
 
-    if (Object.keys(currentTask).length === 0 ) {
-        const taskObj = {
-            id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-            title: `${titleInput.value}`,
-            date: `${dateInput.value}`,
-            description: `${descriptionInput.value}`,
-        }
-    
-        taskArr.unshift(taskObj);
-    } else {
-        currentTask.title = titleInput.value
-        currentTask.date = dateInput.value
-        currentTask.description = descriptionInput.value
+		taskArr.unshift(taskObj);
+	} else {
+		currentTask.title = titleInput.value;
+		currentTask.date = dateInput.value;
+		currentTask.description = descriptionInput.value;
 
-        taskArr.forEach(({ id }, index) => {
-            if (id === currentTask.id) {
-                taskArr.splice(index, 1, currentTask);
-            }
-        });
-    }
+		taskArr.forEach(({ id }, index) => {
+			if (id === currentTask.id) {
+				taskArr.splice(index, 1, currentTask);
+			}
+		});
+	}
 
-    localStorage.setItem("tasks-data", JSON.stringify(taskArr));
-    
-    displayTasks();
-    reset();
-    changeDisplay();
+	localStorage.setItem("tasks-data", JSON.stringify(taskArr));
+
+	displayTasks();
+	reset();
+	changeDisplay();
 };
 
 const displayTasks = () => {
-    tasksOutputContainer.innerHTML = "";
+	tasksOutputContainer.innerHTML = "";
 
-    taskArr.forEach(({ id, title, date, description }) => {
-        tasksOutputContainer.innerHTML += `
+	taskArr.forEach(({ id, title, date, description }) => {
+		tasksOutputContainer.innerHTML += `
             <div class="task-item" id="${id}">
                 <p>Title: ${title}</p>
                 <p>Date: ${date}</p>
@@ -59,46 +61,46 @@ const displayTasks = () => {
                 <button id="deleteBtn" onclick="deleteTask(this)">Delete</button>
             </div>
         `;
-    });
-}
+	});
+};
 
 const updateTask = (updateBtn) => {
-    const idOfTask = updateBtn.parentElement.id;
-    const indexOfTask = getIndex(idOfTask);
+	const idOfTask = updateBtn.parentElement.id;
+	const indexOfTask = getIndex(idOfTask);
 
-    currentTask = taskArr[indexOfTask]
+	currentTask = taskArr[indexOfTask];
 
-    titleInput.value = taskArr[indexOfTask].title
-    dateInput.value = taskArr[indexOfTask].date
-    descriptionInput.value = taskArr[indexOfTask].description
+	titleInput.value = taskArr[indexOfTask].title;
+	dateInput.value = taskArr[indexOfTask].date;
+	descriptionInput.value = taskArr[indexOfTask].description;
 
-    addOrUpdateBtn.innerText = "Update task"
+	addOrUpdateBtn.innerText = "Update task";
 
-    changeDisplay();
-}
+	changeDisplay();
+};
 
 const deleteTask = (deleteBtn) => {
-    const idOfTask = deleteBtn.parentElement.id;
-    const indexOfTask = getIndex(idOfTask);
-    taskArr.splice(indexOfTask, 1);
+	const idOfTask = deleteBtn.parentElement.id;
+	const indexOfTask = getIndex(idOfTask);
+	taskArr.splice(indexOfTask, 1);
 
-    localStorage.setItem("tasks-data", JSON.stringify(taskArr));
+	localStorage.setItem("tasks-data", JSON.stringify(taskArr));
 
-    reset();
-    displayTasks();
-}
+	reset();
+	displayTasks();
+};
 
 const validateInput = () => {
-    const regex = /^\s*/g;
-    titleInput.value = String(titleInput.value).replace(regex, "");
-    
+	const regex = /^\s*/g;
+	titleInput.value = String(titleInput.value).replace(regex, "");
+
 	if (titleInput.value !== "" && dateInput.value !== "") {
 		addOrUpdateTask();
 	} else {
 		alert("Please input a title and date - description is optional");
 		return;
 	}
-}
+};
 
 createNewTaskBtn.addEventListener("click", () => {
 	changeDisplay();
@@ -106,15 +108,19 @@ createNewTaskBtn.addEventListener("click", () => {
 
 formContainer.addEventListener("submit", (e) => {
 	e.preventDefault();
-    validateInput()
+	validateInput();
 });
 
 quitFormBtn.addEventListener("click", () => {
-    if (titleInput.value === "" && dateInput.value === "" && descriptionInput.value === "") {
-        reset()
-        changeDisplay()
-        return
-    }
+	if (
+		titleInput.value === "" &&
+		dateInput.value === "" &&
+		descriptionInput.value === ""
+	) {
+		reset();
+		changeDisplay();
+		return;
+	}
 	quitFormModal.showModal();
 });
 
@@ -134,19 +140,18 @@ const changeDisplay = () => {
 };
 
 const reset = () => {
-    currentTask = {}
+	currentTask = {};
 	titleInput.value = "";
 	dateInput.value = "";
 	descriptionInput.value = "";
-    addOrUpdateBtn.innerText = "Add task"
-
+	addOrUpdateBtn.innerText = "Add task";
 };
 
 const getIndex = (id) => {
-    const task = taskArr.find(task => task.id === id);
-    return taskArr.indexOf(task);
-}
+	const task = taskArr.find((task) => task.id === id);
+	return taskArr.indexOf(task);
+};
 
 if (taskArr.length > 0) {
-    displayTasks();
+	displayTasks();
 }
